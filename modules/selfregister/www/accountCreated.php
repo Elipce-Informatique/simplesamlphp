535d7d30cd1f327439c1549b1ae0bbe8b0d96a1b
MarvinDurot <marvin.dut@gmail.com>
2016-06-29 11:42:18 +0200

    Add new pages for feedback registration

---
+++ modules/selfregister/www/accountCreated.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta name="robots" content="noindex">
+	<title>Compte créé</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="<?= SimpleSAML_Module::getModuleURL('selfregister/img/favicon.ico') ?>" />
+	<!-- CSS -->
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?= SimpleSAML_Module::getModuleURL('selfregister/css/style.css') ?>" rel="stylesheet">
+</head>
+<body>
+<!-- Container -->
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+			<img class="img im-responsive center-block"
+				 src="<?= SimpleSAML_Module::getModuleURL('selfregister/img/logo.jpg') ?>"/>
+			<div class="jumbotron text-center">
+				<h1><span class="glyphicon glyphicon-ok"></span></h1>
+				<p>Votre comte a bien été créé !</p>
+				<p class="text-muted">Vous pouvez modifier vos <em>informations personnelles</em> à tout moment en cliquant <a href="<?= SimpleSAML_Module::getModuleURL('selfregister/index.php') ?>">ici</a>.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<br/>
+<!-- Footer -->
+<footer class="footer">
+	<div class="container">
+		<p class="text-muted">Elipce Informatique &copy;</p>
+	</div>
+</footer>
+<!-- JS -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+</body>
+</html>
