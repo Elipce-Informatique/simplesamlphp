422b62794f068f6ed492da33787b155a61bc92c2
MarvinDurot <marvin.dut@gmail.com>
2016-06-30 11:02:20 +0200

    Update mails #2

--- modules/selfregister/templates/lostPasswordMail.php
+++ modules/selfregister/templates/lostPasswordMail.php
@@ -1,5 +1,5 @@
-<h1>Mot de passe oublié sur l'IdP Elipce</h1>
+<h1>Mot de passe oublié - IdP Elipce</h1>
 <p>Pour réinitialiser votre mot de passe sur le fournisseur d'identité Elipce, suivez le lien suivant :</p>
 <p><a href="<?php echo $this->data['registerurl']; ?>"><?php echo $this->data['registerurl']; ?></a></p>
-<p>Ce lien contient un jeton d'accès sécurisé qui expire au bout de <strong>5 jours</strong>. Passé ce délais il faut renouveler la demande à partir de la page de connexion.</p>
+<p><i>Ce lien contient un jeton d'accès sécurisé qui expire au bout de <strong>5 jours</strong>.</i></p>
 <br/><p>Respectueusement, toute l'équipe d'Elipce.</p>
--- modules/selfregister/templates/registrationMail.php
+++ modules/selfregister/templates/registrationMail.php
@@ -1,7 +1,7 @@
-<h1>Inscription sur l'IdP Elipce</h1>
+<h1>Inscription - IdP Elipce</h1>
 <p>Bonjour, nous vous remercions pour votre inscription !</p>
 <p>Cette email vous a été envoyé afin de pouvoir vérifier la validité de votre adresse : <pre><?php echo $this->data['email']; ?></pre></p>
 <p>Pour continuer votre inscription, nous vous demandons de cliquer sur ce lien suivant :</p>
 <p><a href="<?php echo $this->data['registerurl']; ?>"><?php echo $this->data['registerurl']; ?></a></p>
-<p>Ce lien contient un jeton d'accès sécurisé qui expire au bout de <strong>5 jours</strong>. Passé ce délais il faut renouveler la demande à partir de la page de d'inscription.</p>
+<p><i>Ce lien contient un jeton d'accès sécurisé qui expire au bout de <strong>5 jours</strong>.</i></p>
 <br/><p>Respectueusement, toute l'équipe d'Elipce.</p>
