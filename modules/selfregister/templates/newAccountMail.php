f483a28494a027d811f7acc5e50505976f4b9387
MarvinDurot <marvin.dut@gmail.com>
2016-07-18 11:34:48 +0200

    Update new account mail title

--- modules/selfregister/templates/newAccountMail.php
+++ modules/selfregister/templates/newAccountMail.php
@@ -1,4 +1,4 @@
-<h1>Nouvelle inscription</h1>
+<h1>Nouvelle inscription !</h1>
 <p>Un utilisateur vient de s'inscrire avec l'email suivant :
     <pre><?php echo $this->data['email']; ?></pre>
 </p>
